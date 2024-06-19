ee382c8ccc3ae665748d75e4f78719d65c2cf9d8
jesusrobot0 <jesusrobot9000@icloud.com>
2024-06-18 22:43:14 -0500

    Move the upcoming page to the root and add minor design improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
-import { redirect } from "next/navigation";
+import Image from "next/image";
+import { italicFont } from "@/config/fonts";
 
 export default function HomePage() {
-  redirect("/proximamente");
+  return (
+    <main className="w-screen h-screen flex justify-center items-center text-[#222] bg-[#f8f1e7]">
+      <section className="flex flex-col gap-4 text-center font-light">
+        <Image
+          src="/icons/sun.png"
+          alt="Logo del sitio que representa sol sonriente con gafas de sol, simbolizando alegría y energía positiva."
+          width={180 / 2}
+          height={180 / 2}
+          className="mx-auto mb-5"
+        />
+        <h1 className="flex flex-col gap-4 text-3xl sm:text-4xl sm:flex-row">
+          <span className="font-extrabold uppercase">
+            Ojinaga
+            <span className="text-4xl leading-[0px] text-red-500 font-extrabold">
+              .
+            </span>
+            MX
+          </span>
+          <span className="hidden font-extralight sm:block"> | </span>
+          <span className="italic">Próximamente</span>
+        </h1>
+
+        <h2 className="text-xl sm:text-2xl">
+          Explora, Conecta
+          <span className={`${italicFont.className}`}> & </span>
+          <span className="font-semibold">Disfruta</span>
+        </h2>
+
+        <p className="text-xl sm:text-2xl mt-4">
+          Para saber más{" "}
+          <a
+            href="mailto:hola@ojinaga.mx"
+            className="font-bold hover:underline"
+          >
+            hola@ojinaga.mx
+          </a>
+        </p>
+      </section>
+    </main>
+  );
 }
---
+++ src/config/fonts.ts
@@ -0,0 +1,6 @@
+import { Cormorant_Garamond } from "next/font/google";
+
+export const italicFont = Cormorant_Garamond({
+  weight: ["300"],
+  subsets: ["latin"],
+});
